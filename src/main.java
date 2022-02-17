0103f84d1174c14e5ee9659ac64fe80479575284
uinipu <91496899+uinipu@users.noreply.github.com>
2022-02-17 17:24:56 +0200

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,19 @@
+public class main {
+    public static void main(String args[]){
+        System.out.println("\n"+
+                "At lunchtime I bought a huge orange—\n" +
+                "The size of it made us all laugh.\n" +
+                "I peeled it and shared it with Robert and Dave—\n" +
+                "They got quarters and I had a half.\n" +
+                "\n" +
+                "And that orange, it made me so happy,\n" +
+                "As ordinary things often do\n" +
+                "Just lately. The shopping. A walk in the park.\n" +
+                "This is peace and contentment. It’s new.\n" +
+                "\n" +
+                "The rest of the day was quite easy.\n" +
+                "I did all the jobs on my list\n" +
+                "And enjoyed them and had some time over.\n" +
+                "I love you. I’m glad I exist.");
+    }
+}
